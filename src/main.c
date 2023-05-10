f340cd510ed89709d22aa648b5a9cb798964db16
hlord2000 <helmut@helmutlord.com>
2023-05-10 16:35:41 -0400

    Adjusted to use async UART

--- src/main.c
+++ src/main.c
@@ -16,45 +16,15 @@
 
 #define MSG_SIZE 32
 
-/* queue to store up to 10 messages (aligned to 4-byte boundary) */
-K_MSGQ_DEFINE(uart_msgq, MSG_SIZE, 10, 4);
 
 static const struct device *const uart_dev = DEVICE_DT_GET(UART_DEVICE_NODE);
-
-/* receive buffer used in UART ISR callback */
-static char rx_buf[MSG_SIZE];
-static int rx_buf_pos;
 
 /*
  * Read characters from UART until line end is detected. Afterwards push the
  * data to the message queue.
  */
-void serial_cb(const struct device *dev, void *user_data)
-{
-	uint8_t c;
-
-	if (!uart_irq_update(uart_dev)) {
-		return;
-	}
-
-	while (uart_irq_rx_ready(uart_dev)) {
-
-		uart_fifo_read(uart_dev, &c, 1);
-
-		if ((c == '\n' || c == '\r') && rx_buf_pos > 0) {
-			/* terminate string */
-			rx_buf[rx_buf_pos] = '\0';
-
-			/* if queue is full, message is silently dropped */
-			k_msgq_put(&uart_msgq, &rx_buf, K_NO_WAIT);
-
-			/* reset the buffer (it was copied to the msgq) */
-			rx_buf_pos = 0;
-		} else if (rx_buf_pos < (sizeof(rx_buf) - 1)) {
-			rx_buf[rx_buf_pos++] = c;
-		}
-		/* else: characters beyond buffer size are dropped */
-	}
+uart_callback_t uart_callback(const struct device *dev, struct uart_event *evt, void *user_data) {
+	printk("Event: %d\n", evt->type);
 }
 
 /*
@@ -62,25 +32,36 @@ void serial_cb(const struct device *dev, void *user_data)
  */
 void print_uart(char *buf)
 {
+	int err;
 	int msg_len = strlen(buf);
-
-	for (int i = 0; i < msg_len; i++) {
-		uart_poll_out(uart_dev, buf[i]);
+	err = uart_tx(uart_dev, buf, msg_len, SYS_FOREVER_MS);
+	if (err) {
+		printk("UART TX error: %d\n", err);
 	}
 }
 
 void main(void)
 {
-	char tx_buf[MSG_SIZE];
 
 	if (!device_is_ready(uart_dev)) {
 		printk("UART device not found!");
 		return;
 	}
 
+	char tx_buf[MSG_SIZE];
+
+	uart_rx_enable(uart_dev, tx_buf, MSG_SIZE, 1000);
+
+	int err;
+
+	err = uart_callback_set(uart_dev, uart_callback, NULL);
+	if (err) {
+		printk("Cannot set UART callback: %d\n", err);
+		return;
+	}
+
+
 	/* configure interrupt and callback to receive data */
-	uart_irq_callback_user_data_set(uart_dev, serial_cb, NULL);
-	uart_irq_rx_enable(uart_dev);
 
 	print_uart("Hello! I'm your echo bot.\r\n");
 	print_uart("Tell me something and press enter:\r\n");
